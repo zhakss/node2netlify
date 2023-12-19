518e221486b2a801b9a4f77d88a6c47dd27b882f
zhakss <zakyarrd@gmail.com>
2023-12-19 10:07:44 +0700

    node2netlify

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', { message: 'Hello, Express and EJS!' });
+});
+
+app.listen(port, () => {
+  console.log(`server is runninh`);
+});
